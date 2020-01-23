cbe1297be17851404096a9e91525e09c165f4947
Avinash Subhash Kolte <akolte@altimetrik.com>
2020-01-23 12:56:02 +0530

    first commit

--- force-app/main/default/lwc/fileUpload/fileUpload.js
+++ force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -1,3 +1,15 @@
+/* eslint-disable no-console */
 import { LightningElement } from 'lwc';
 
-export default class FileUpload extends LightningElement {}
+export default class FileUpload extends LightningElement {
+    handleFilesChange(event)
+    {
+        const fileData=event.target.value;
+        console.log("file data:-->"+fileData);
+    }
+
+    handleClick()
+    {
+        console.log("inside button click-->");
+    }
+}
